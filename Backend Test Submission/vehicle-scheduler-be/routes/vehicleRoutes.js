12f6702ec47a2e30b1f0dca8be43b15deadef569
Sivag28 <siva.g282005@gmail.com>
2026-07-02 12:54:56 +0530

    initial Commit

--- Backend Test Submission/vehicle-scheduler-be/routes/vehicleRoutes.js
+++ Backend Test Submission/vehicle-scheduler-be/routes/vehicleRoutes.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
-const vehicleRoutes = require("./routes/vehicleRoutes");
-const app = express();
-app.use(express.json());
-app.use("/api/schedule", vehicleRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Vehicle scheduler running on port ${PORT}`);
-});
+const router = express.Router();
+
+const vehicleController = require("../controllers/vehicleController");
+
+router.get("/", vehicleController.generateSchedule);
+
+module.exports = router;
